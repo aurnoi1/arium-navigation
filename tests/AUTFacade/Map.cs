5f58619c5847b4a13c849b0f99f53a325775bde5
Aurelien Noisette <aurelien.noisette@exfo.com>
2019-12-03 08:28:10 -0500

    Remove CancellationToken from Map.

--- tests/AUTFacade/Map.cs
+++ tests/AUTFacade/Map.cs
@@ -23,7 +23,6 @@ namespace AUT.Facade
 
         public readonly R RemoteDriver;
 
-        private readonly CancellationToken DefaultTimeout;
 
         /// <summary>
         /// The nodes of INavigables forming the Graph.
@@ -36,14 +35,13 @@ namespace AUT.Facade
 
         private readonly ILog log;
 
-        public Map(R remoteDriver, ILog log, CancellationToken DefaultTimeout)
+        public Map(R remoteDriver, ILog log)
         {
             DynamicNeighbors = new HashSet<DynamicNeighbor>();
             this.log = log;
             RemoteDriver = remoteDriver;
             Nodes = GetNodesByReflection<R>(Assembly.GetExecutingAssembly());
             Graph = new Graph(Nodes);
-            this.DefaultTimeout = DefaultTimeout;
             AddDynamicNeighbors();
         }
 
