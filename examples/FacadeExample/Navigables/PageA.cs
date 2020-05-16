303e360da7e6313520896d6dacf343538244dc43
aurnoi1 <11244735+aurnoi1@users.noreply.github.com>
2020-05-16 14:49:57 -0400

    Add FacadeExample.

---
+++ examples/FacadeExample/Navigables/PageA.cs
@@ -0,0 +1,54 @@
+﻿using Arium;
+using Arium.Enums;
+using Arium.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace FacadeExample.Navigables
+{
+    public class PageA : NavigablePublisher, INavigable
+    {
+        public Dictionary<INavigable, Action<CancellationToken>> GetActionToNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public HashSet<DynamicNeighbor> GetDynamicNeighbors()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Notify observers of a specific State's value.
+        /// </summary>
+        /// <typeparam name="T">The State's value type.</typeparam>
+        /// <param name="stateName">The state name.</param>
+        /// <returns>The State.</returns>
+        public override IState<T> PublishState<T>(StatesNames stateName)
+        {
+            bool exist = true;
+            IState<T> state = stateName switch
+            {
+                StatesNames.Exist => (IState<T>)StateFactory.Create(this, StatesNames.Exist, exist),
+                StatesNames.Ready => (IState<T>)StateFactory.Create(this, StatesNames.Ready, exist),
+                _ => throw new ArgumentException($"Undefined {nameof(StatesNames)}: {stateName}."),
+            };
+
+            NotifyObservers(state);
+            return state;
+        }
+
+        /// <summary>
+        /// Notify observers of the current INavigable status.
+        /// </summary>
+        /// <returns>The published current INavigable status.</returns>
+        public override INavigableStatus PublishStatus()
+        {
+            var exist = true;
+            var enable = true;
+            var navigableStatus = new NavigableStatus(this, exist, enable);
+            return navigableStatus;
+        }
+    }
+}
---
+++ examples/FacadeExample/Navigables/PageC.cs
@@ -0,0 +1,45 @@
+﻿using Arium;
+using Arium.Enums;
+using Arium.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace FacadeExample.Navigables
+{
+    public class PageC : NavigablePublisher, INavigable
+    {
+        public Dictionary<INavigable, Action<CancellationToken>> GetActionToNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public HashSet<DynamicNeighbor> GetDynamicNeighbors() => new HashSet<DynamicNeighbor>();
+
+        /// <summary>
+        /// Notify observers of a specific State's value.
+        /// </summary>
+        /// <typeparam name="T">The State's value type.</typeparam>
+        /// <param name="stateName">The state name.</param>
+        /// <returns>The State.</returns>
+        public override IState<T> PublishState<T>(StatesNames stateName)
+        {
+            return stateName switch
+            {
+                StatesNames.Exist => (IState<T>)StateFactory.Create(this, StatesNames.Exist, true),
+                StatesNames.Ready => (IState<T>)StateFactory.Create(this, StatesNames.Ready, true),
+                _ => throw new ArgumentException("Unknown StatesNames"),
+            };
+        }
+
+        /// <summary>
+        /// Notify observers of the current INavigable status.
+        /// </summary>
+        /// <returns>The published current INavigable status.</returns>
+        public override INavigableStatus PublishStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
