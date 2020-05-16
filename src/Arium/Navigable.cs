9411c4288027b43cf55234f98a425a6b61ddd17c
aurnoi1 <11244735+aurnoi1@users.noreply.github.com>
2020-05-16 15:31:51 -0400

    Rename NavigablePublisher to Navigable.

--- src/Arium/Navigable.cs
+++ src/Arium/Navigable.cs
@@ -9,11 +9,24 @@ using System.Threading;
 namespace Arium
 {
     /// <summary>
-    /// Abstract implementation of INavigablePublisher.
+    /// Abstract implementation of <see cref="Navigable"/>.
     /// </summary>
-    public abstract class NavigablePublisher : INavigable
+    public abstract class Navigable : INavigable
     {
         private readonly List<WeakReference<INavigableObserver>> observers = new List<WeakReference<INavigableObserver>>();
+
+        /// <summary>
+        /// The function returning the Exist status.
+        /// </summary>
+        /// <returns>The Exists status.</returns>
+        public abstract Func<bool> Exist();
+
+        /// <summary>
+        /// The function returning the Ready status.
+        /// </summary>
+        /// <returns>The Ready status.</returns>
+        public abstract Func<bool> Ready();
+
 
         /// <summary>
         /// Register the INavigableObserver as a WeakReference.
@@ -91,7 +104,14 @@ namespace Arium
         /// Notify observers of the current INavigable status.
         /// </summary>
         /// <returns>The published current INavigable status.</returns>
-        public abstract INavigableStatus PublishStatus();
+        public virtual INavigableStatus PublishStatus()
+        {
+            bool exist = PublishState<bool>(StatesNames.Exist).Value;
+            bool ready = PublishState<bool>(StatesNames.Exist).Value;
+            NavigableStatus status = new NavigableStatus(this, exist, ready);
+            NotifyObservers(status);
+            return status;
+        }
 
         /// <summary>
         /// Notify observers of a specific State's value.
@@ -99,12 +119,23 @@ namespace Arium
         /// <typeparam name="T">The State's value type.</typeparam>
         /// <param name="stateName">The state name.</param>
         /// <returns>The State.</returns>
-        public abstract IState<T> PublishState<T>(StatesNames stateName);
+        public virtual IState<T> PublishState<T>(StatesNames stateName)
+        {
+            State<bool> state = stateName switch
+            {
+                StatesNames.Exist => StateFactory.Create(this, StatesNames.Exist, Exist().Invoke()),
+                StatesNames.Ready => StateFactory.Create(this, StatesNames.Ready, Ready().Invoke()),
+                _ => throw new ArgumentException($"Undefined {nameof(StatesNames)}: {stateName}."),
+            };
+
+            NotifyObservers(state);
+            return (IState<T>)state;
+        }
 
         /// <summary>
-        /// Gets a Dictionary of action to go to the next INavigable.
+        /// Gets a Dictionary of actions to go to the next Navigable.
         /// </summary>
-        /// <returns>A Dictionary of action to go to the next INavigable.</returns>
+        /// <returns>A Dictionary of actions to go to the next Navigable.</returns>
         public virtual Dictionary<INavigable, Action<CancellationToken>> GetActionToNext()
         {
             return new Dictionary<INavigable, Action<CancellationToken>>();
